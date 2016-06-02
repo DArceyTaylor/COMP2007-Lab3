d7af01734208adc1c6be0daaf5e280666e6c1d73
DArceyTaylor <darceytaylor05@gmail.com>
2016-06-02 19:30:01 -0400

    added the cancel and save students button with functionality

---
+++ COMP2007-Lab3/StudentDetails.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using COMP2007_Lab3.Models;
+using System.Web.ModelBinding;
+
+namespace COMP2007_Lab3
+{
+    public partial class StudentDetails : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void CancelButton_Click(object sender, EventArgs e)
+        {
+            // redirect back to students page
+            Response.Redirect("~/Students.aspx");
+        }
+
+        
+        protected void SaveButton_Click(object sender, EventArgs e)
+        {
+            // use EF to connect to the server
+            using(DefaultConnection db = new DefaultConnection())
+            {
+                // use the Student model to create a new student object and
+                // save a new record
+                Student newStudent = new Student();
+
+                // add data to the new student record
+                newStudent.LastName = LastNameTextBox.Text;
+                newStudent.FirstMidName = FirstNameTextBox.Text;
+                newStudent.EnrollmentDate = Convert.ToDateTime(EnrollmentDateTextBox.Text);
+
+                // use LINQ to ADO.NET to add / insert new student to the db
+                db.Students.Add(newStudent);
+
+                // save our changes
+                db.SaveChanges();
+
+                // redirect back to the updated students page
+                Response.Redirect("~/Students.aspx");
+            }
+        }
+    }
+}
